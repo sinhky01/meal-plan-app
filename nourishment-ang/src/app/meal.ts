c29945a2f658c05618c28814a915af28f556e3e8
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-08-01 09:40:06 -0400

    Added meal class file, and calendar service to be used later with http request

---
+++ nourishment-ang/src/app/meal.ts
@@ -0,0 +1,7 @@
+// The purpose of this class is to be used with calendar service component, at least temporarily
+export class Meal {
+  id: number;
+  date: Date;
+  mealType: String; // i.e. breakfast, lunch, etc.
+  
+}
