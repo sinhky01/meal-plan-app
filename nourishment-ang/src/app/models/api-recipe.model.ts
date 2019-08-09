d403b9a3266e06256b467905032f5e838f00f083
sinhky01 <kyle.sinhart@gmail.com>
2019-08-09 09:43:59 -0400

    commit for the master merge

--- nourishment-ang/src/app/models/api-recipe.model.ts
+++ nourishment-ang/src/app/models/api-recipe.model.ts
@@ -2,7 +2,4 @@ import { indivRecipe } from './indiv-recipe.model';
 
 export class apiRecipe {
     meals: indivRecipe[];
-    
-     
-    
 }
---
+++ nourishment-ang/src/app/services/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { HttpErrorResponse, HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    error: any;
+    url: string = 'https://localhost:4200';
+    //url: string = 'https://localhost:9595';
+
+
+    constructor(private http: HttpClient) { }
+
+    public fetchUser(id: number): Observable<User>{
+        return this.http.get<User>(`${this.url}/api/v1/user/${id}`).pipe(catchError(this.handleError));
+
+    }
+    public checkLogin(username: string, password: string): Observable<User>{
+        return this.http.post<User>(`${this.url}/api/v1/user/login`,JSON.stringify(new User(username,password))).pipe(catchError(this.handleError));
+    }
+    public getPreferences(id: number): Observable<Array<string>>{
+        return this.http.get<Array<string>>(`${this.url}/api/v1/preferences/preferences/${id}`).pipe(catchError(this.handleError));
+    }
+    public getHistory(id: number):Observable<Array<string>>{
+        return this.http.get<Array<string>>(`${this.url}/api/v1/history/user/${id}/favorited`).pipe(catchError(this.handleError));
+    }
+    public handleError(error: HttpErrorResponse){
+        return Observable.throw(error.statusText);
+    }
+
+}
